fa098b68a348908508ea5bab48d555e0bcbbea3d
ABISHEK-2908 <abipappu07@gmail.com>
2026-02-01 11:55:56 +0530

    modi

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ public class add {
     public static void main(String[] args) {
         int a = 11000;
         int b = 200;
-        int c = 30;
+        int c = 300;
         int d = a+b+c;
        System.out.println(d);
     }
